ed8fa4e941a3a3263b084e79f5c2f9edcb553bac
LuuKeeTZ <100455507+LuuKeeTZ@users.noreply.github.com>
2022-05-25 12:48:54 -0300

    primeiro commit

---
+++ jantinha.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class jantinha {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        s
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
