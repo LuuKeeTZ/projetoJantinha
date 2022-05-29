468f8f44fa53d233d938ae8418ea3222922735f6
LuuKeeTZ <100455507+LuuKeeTZ@users.noreply.github.com>
2022-05-29 11:05:14 -0300

    Update jantinha.java

--- jantinha.java
+++ jantinha.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
-
-public class jantinha {
+public class projetoJantinha {
     public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
-        s
+      Scanner leia = new Scanner(System.in);  
+      
+      int maxLin = 3, maxCol = 3;
+      int [][] valores = new int [maxLin][maxCol];
+      
+      for(int i = 0; i < maxLin; i++){
+          
+        for(int j = 0;j < maxCol; j++){
+              valores[i][j] = leia.nextInt();
+              
+          }
+      }
         
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+        
+        
+        
+        
     }
     
 }
+
