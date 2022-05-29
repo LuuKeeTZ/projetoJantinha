ca1781f7b48bad0b4920ef72b4ce68b2133d8c3e
LuuKeeTZ <100455507+LuuKeeTZ@users.noreply.github.com>
2022-05-29 11:36:01 -0300

    Nota de Atualização
    Coloquei o if para ler os pratos e os valores de cada coisa do prato, porem esta dando erro " Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3 "

--- jantinha.java
+++ jantinha.java
@@ -3,15 +3,72 @@ public class projetoJantinha {
     public static void main(String[] args) {
       Scanner leia = new Scanner(System.in);  
       
-      int maxLin = 3, maxCol = 3;
+      int maxLin = 3, maxCol = 4;
       int [][] valores = new int [maxLin][maxCol];
       
-      for(int i = 0; i < maxLin; i++){
-          
-        for(int j = 0;j < maxCol; j++){
-              valores[i][j] = leia.nextInt();
-              
+      for(int i = 0; i < maxCol; i++){
+          if(i == 0 ){
+              System.out.println("Digite o valor de arroz dos pratos:");  
           }
+          if(i == 1){
+              System.out.println("Digite o valor do carne dos pratos:");
+          }
+          if(i == 2){
+              System.out.println("Digite o valor da salada dos pratos:");
+          }
+          if(i == 3){
+              System.out.println("Digite o valor do tropeiro dos pratos:");
+          }
+          
+      for(int j = 0;j < maxLin; j++){
+          if(i == 0 && j == 0 ){
+              System.out.println("Prato 1");
+          }
+          if(i == 0 && j == 1){
+              System.out.println("Prato 2");
+          }
+          if(i == 0 && j == 2){
+              System.out.println("Prato 3");
+          }
+          if(i == 1 && j == 0){
+              System.out.println("Prato 1");
+          }
+          if(i == 1 && j == 1){
+              System.out.println("Prato 2");
+          }
+          if(i == 1 && j == 2){
+              System.out.println("prato 3");
+          }
+          if(i == 2 && j == 0){
+              System.out.println("Prato 1");
+          }
+          if(i == 2 && j == 1){
+              System.out.println("Prato 2");
+          }
+          if(i == 2 && j == 2){
+              System.out.println("Prato 3");
+          }
+          if(i == 3 && j == 0){
+              System.out.println("Prato 1");
+          }
+          if(i == 3 && j == 1){
+              System.out.println("Prato 2");
+          }
+          if(i == 3 && j == 2){
+              System.out.println("Prato 3");
+          }
+          
+          valores[i][j] = leia.nextInt();
+              
+          
+      
+      
+      
+      
+      
+      
+      
+      }
       }
         
         
@@ -22,4 +79,3 @@ public class projetoJantinha {
     }
     
 }
-
